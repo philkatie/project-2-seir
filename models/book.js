2af589391bcee163cd0e9622004fb6f88fde501b
philkatie <kaphillips219@gmail.com>
2022-04-07 17:29:27 -0500

    first pass at models

---
+++ models/book.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        default: 5
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    userName: String,
+    userAvatar: String
+}, {
+    timestamps: true
+});
+
+const bookSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    releaseYear: {
+        type: Number
+    },
+    pageCount: {
+        type: Number,
+        min: 1
+    },
+    imageURL: {
+        type: String
+    },
+    genre: {
+        type: String,
+        enum: ['list of genres']
+    },
+    onTbr: [{type: Schema.Types.ObjectId, ref: 'User'}],
+    usersReadubg: [{type: Schema.Types.ObjectId, ref: 'User'}],
+    reviews: [reviewSchema]
+});
+
+module.exports = mongoose.model('Book', bookSchema);
