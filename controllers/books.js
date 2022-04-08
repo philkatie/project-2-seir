99b12179d8f0d647d6cbab4264eedb480065819c
philkatie <kaphillips219@gmail.com>
2022-04-08 16:40:28 -0500

    routes to all books, book details, new book working

--- controllers/books.js
+++ controllers/books.js
@@ -2,14 +2,35 @@ const Book = require('../models/book');
 
 
 module.exports = {
-
+    index,
+    new: newBook,
+    show
 };
 
 // index function - renders All Books index.ejs
+function index(req, res) {
+    Book.find({}, function(err, books) {
+        res.render('books/index', {
+            books,
+            title: "All Books"
+        });
+    });
+};
 
 // show function - renders Book Detail show.ejs
+function show(req, res) {
+    Book.findById(req.params.id, function(err, book) {
+        res.render('books/show', {
+            title: 'Book Details'
+        });
+    });
+};
 
 // new: newBook function - renders New Book new.ejs
+function newBook(req, res) {
+    res.render('books/new', {
+        title: 'New Book'
+    });
+};
 
 // create function - lets a user add a new book to the DB
-
--- routes/books.js
+++ routes/books.js
@@ -4,13 +4,14 @@ const booksCtrl = require('../controllers/books');
 const isLoggedIn = require('../config/auth');
 
 // load books/index.ejs
-// router.get('/', booksCtrl.index);
+router.get('/', booksCtrl.index);
 
 // load books/new.ejs
+router.get('/new', booksCtrl.new);
 // router.get('/new', isLoggedIn, booksCtrl.new);
 
 // load books/show.ejs
-// router.get('/:id', booksCtrl.show);
+router.get('/:id', booksCtrl.show);
 
 // add new book to db
 // router.post('/', isLoggedIn, booksCtrl.create);
