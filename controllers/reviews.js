0c97e7bd47404abbb93b4eac60976dfc32d84d22
philkatie <kaphillips219@gmail.com>
2022-04-08 18:27:32 -0500

    making progress on reviews, kind of

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,9 +1,21 @@
 const Book = require('../models/book');
 
 module.exports = {
-
+    create
 };
 
 // delete: deleteReview function deletes a review by the user
 
 // create function, allows a user to create a new review
+function create(req, res) {
+    Book.findById(req.params.id, function(err, book) {
+        req.body.user = req.user._id;
+        req.body.userName = req.user.name;
+        req.body.userAvatar = req.user.avatar;
+
+        book.reviews.push(req.body);
+        book.save(function(err) {
+            res.redirect(`/books/${book._id}`);
+        });
+    });
+};
--- routes/reviews.js
+++ routes/reviews.js
@@ -4,6 +4,7 @@ const reviewsCtrl = require('../controllers/reviews');
 const isLoggedIn = require('../config/auth');
 
 // lets user add a review
+router.post('/books/:id/reviews', reviewsCtrl.create);
 // router.post('/books/:id/reviews', isLoggedIn, reviewsCtrl.create);
 
 // lets user delete their review
