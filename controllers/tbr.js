629e5cb7c282ec213fdb75940155d2280b2db976
philkatie <kaphillips219@gmail.com>
2022-04-13 13:35:58 -0500

    remove from tbr works now

--- controllers/tbr.js
+++ controllers/tbr.js
@@ -1,15 +1,42 @@
 const Book = require('../models/book');
 
 module.exports = {
-    create
+    create,
+    delete: deleteTbr
 }
 
 // addToTbr function - adds book to user's TBR
 function create(req, res) {
     Book.findById(req.params.id, function(err, book) {
+        console.log(book);
         book.onTbr.push(req.user._id);
         book.save(function(err) {
             res.redirect(`/books/${book._id}`);
+        });
+    });
+};
+
+// remove from Tbr
+// function deleteTbr(req, res) {
+//     Book.findById(req.params.id, function(err, book) {
+//         console.log(book);
+//         });
+// };
+
+function deleteTbr(req, res) {
+    Book.findById(req.params.id, function(err, book) {
+        console.log(book);
+        console.log(`${req.user._id} <-- user id`);
+        console.log(book.onTbr);
+        let i = book.onTbr.indexOf(req.user._id);
+        console.log(i);
+        book.onTbr.splice(i, 1);
+        book.save(function(err, b) {
+            if(err) {
+                console.log(err)
+            }
+            console.log(book.onTbr);
+            res.redirect(`/books/${req.params.id}`);
         });
     });
 };
--- routes/tbr.js
+++ routes/tbr.js
@@ -4,5 +4,6 @@ const tbrCtrl = require('../controllers/tbr')
 
 // add book to user's tbr
 router.post('/books/:id/tbr', tbrCtrl.create);
+router.delete('/books/:id/tbr/:userId', tbrCtrl.delete);
 
 module.exports = router;
